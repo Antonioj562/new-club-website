118d3f7b25d25eb386996055daa4f33bbb6d6044
Tony <antoniojloyola@gmail.com>
2025-09-28 14:32:27 -0700

    Added deploy file for github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // If testing locally commet out the line below
+  base: '/new-club-website/',
 })
