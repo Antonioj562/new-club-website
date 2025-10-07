014cbfea3aec31b6ec1753cf0e27d11409419a05
antonioj562 <antoniojloyola@gmail.com>
2025-10-07 13:26:36 -0700

    Updated styling for footer, nav, events, home - added base html for contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,18 @@
+import "../styles/contact.css";
 
 export const Contact = () => {
     return (
-        <div className="contact-container">
-        </div>
-    )
+        <section className="contact-container">
+            <div className="contact-title">
+                <h1>CONTACT US</h1>
+                <h3>ONLINE INQUIRY</h3>
+            </div>
+            <div className="contact-forms">
+                <input type="text" id="contact-name" name="Name" placeholder="Name"/>
+                <input type="text" id="contact-email" name="Email" placeholder="Email"/>
+                <input type="text" id="contact-number" name="Number" placeholer="Phone Number"/>
+                <input type="text" id="contact-message" name="Message" placeholer="Message"/>
+            </div>
+        </section>
+    ) 
 }
